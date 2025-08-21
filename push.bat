85e00f9d8fa8235283ccf1ee52c9eb6577e400cb
Abdulkohar-design <your-email@example.com>
2025-08-21 23:36:55 +0700

    Initial commit: Reaction Caption Generator

---
+++ push.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Adding remote repository...
+git remote add origin https://github.com/vdmax03/reactioncaptiongenerator.git
+echo.
+echo Pushing to GitHub...
+git push -u origin main
+echo.
+echo Done!
+pause
